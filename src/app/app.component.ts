9c668e3f7480b56867d7a21e20197fad8b4e9d37
Rens Jaspers <rens.jaspers@gmail.com>
2023-08-19 15:35:31 +0200

    feat: make animation duration configurable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
 import { SPONSORS } from './sponsors';
 
 @Component({
@@ -10,6 +10,17 @@ import { SPONSORS } from './sponsors';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   readonly sponsors = SPONSORS;
+  animationDuration = '60s';
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      if (params['animationDuration']) {
+        this.animationDuration = params['animationDuration'];
+      }
+    });
+  }
 }
